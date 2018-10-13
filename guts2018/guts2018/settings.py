9262193eff1959cd55c74eabfed75006a50b685a
krisi6012 <krisi6012@gmail.com>
2018-10-13 01:27:42 +0100

    static to add

--- guts2018/guts2018/settings.py
+++ guts2018/guts2018/settings.py
@@ -14,7 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+MEDIA_DIR = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'guts2018.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,7 +73,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'guts2018.wsgi.application'
-
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
